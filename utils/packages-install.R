3daec5852348aa6f981932c0677820bd216f8491
Holger Döring <doering@uni-bremen.de>
2023-08-07 20:35:02 +0200

    Add lock files with renv

--- utils/packages-install.R
+++ utils/packages-install.R
@@ -9,3 +9,6 @@ deps <- unique(renv::dependencies()[["Package"]])
 
 # install project packages
 pak::pkg_install(deps, ask = FALSE)
+
+# create lock file of versions for all packages
+renv::snapshot()
--- utils/packages-upgrade.R
+++ utils/packages-upgrade.R
@@ -3,3 +3,6 @@ deps <- unique(renv::dependencies()[["Package"]])
 
 # upgrade project packages
 pak::pkg_install(deps, ask = FALSE, upgrade = TRUE)
+
+# create lock file of versions for all packages
+renv::snapshot()
