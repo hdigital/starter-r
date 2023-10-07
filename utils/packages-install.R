4906c2563603374d576325fca76bcccff2422ea0
Holger Döring <doering@uni-bremen.de>
2023-10-07 13:41:05 +0200

    Add more explicit install options

--- utils/packages-install.R
+++ utils/packages-install.R
@@ -8,7 +8,8 @@ pak::pkg_install("renv", ask = FALSE)
 deps <- unique(renv::dependencies()[["Package"]])
 
 # install project packages
-pak::pkg_install(deps, ask = FALSE)
+pak::pkg_install(deps, ask = FALSE, upgrade = FALSE)
+# pak::lockfile_install()
 
 # create lock file of versions for all packages
 pak::lockfile_create(deps)
