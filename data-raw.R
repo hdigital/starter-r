6e6da0f2b7fe0436a50ef8c3824b35fec6317307
Holger Döring <doering@uni-bremen.de>
2024-07-12 17:45:36 +0200

    Add 'data-raw' script and folder

---
+++ data-raw.R
@@ -0,0 +1,12 @@
+# Create datasets used in project.
+# Use source files in "data-raw" folder, ignored by git.
+# Save data in "data" folder. Check user license before sharing.
+
+library(tidyverse)
+
+
+## Dataset ----
+
+# dt_raw <- read_csv("data-raw/dt-data.csv")
+# dt <- dt_raw
+# write_csv("data/dt_source.csv")
