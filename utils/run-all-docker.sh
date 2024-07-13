e8c3a6a338d770ea25040b125a38e3f16af492a6
hdigital <3199522+hdigital@users.noreply.github.com>
2024-07-13 22:17:41 +0200

    Add 'run-all' shell script (#7)

---
+++ utils/run-all-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run and render all project R scripts in Rocker container
+
+set -e
+
+docker compose up -d
+
+docker exec rocker-starter \
+  /bin/bash -c \
+  "cd /home/rstudio/ && \
+   Rscript utils/run-all.R > 'utils/run-all.log'"
+
+docker compose down
--- utils/run-all.R
+++ utils/run-all.R
@@ -3,14 +3,14 @@ library(fs)
 library(purrr)
 
 
-## Lock package versions ----
+## Use locked package versions ----
 
-# get packages used in project folder
-deps <- unique(renv::dependencies()[["Package"]])
-
-# create lock file of versions for all packages
-pak::lockfile_create(deps)
-
+if (file_exists("pkg.lock")) {
+  pak::lockfile_install()
+} else {
+  deps <- unique(renv::dependencies()[["Package"]])
+  pak::lockfile_create(deps)
+}
 
 ## Format and check code ----
 
@@ -33,7 +33,7 @@ rmd_scripts <- dir_ls(".", glob = "*.Rmd", recurse = 1)
 map(rmd_scripts, rmarkdown::render)
 
 # render Quarto notebooks in project folder
-system("quarto render *.qmd")
+system("quarto render --cache-refresh *.qmd")
 
 # remove Rplots created with print()
 if (file_exists("Rplots.pdf")) {
