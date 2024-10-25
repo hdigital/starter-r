b90476d1902a1008833e406cdaab0bfc2b385c74
hdigital <3199522+hdigital@users.noreply.github.com>
2024-10-25 10:10:37 +0000

    Revise R packages installation with 'pak'
    - always run 'pak' install
    - create lock file if none exists
    - use 'library(pak)' in update script

--- utils/r-packages-update.R
+++ utils/r-packages-update.R
@@ -1,15 +1,17 @@
+library(pak)
+
 # install 'pak' to use modern package installer
 if (!"pak" %in% installed.packages()) {
   install.packages("pak", repos = getOption("repos")[[1]])
 }
 
 # get packages used in project folder
-pak::pkg_install("renv", ask = FALSE, upgrade = FALSE)
+pkg_install("renv", ask = FALSE, upgrade = FALSE)
 deps <- unique(renv::dependencies()[["Package"]])
 
 # install project packages
-pak::pkg_install(deps, ask = FALSE, upgrade = TRUE)
+pkg_install(deps, ask = FALSE, upgrade = TRUE)
 # pak::lockfile_install(update = FALSE)
 
 # create lock file of versions for all packages
-pak::lockfile_create(deps)
+lockfile_create(deps)
