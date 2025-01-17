aa4c66032983b0b3a4cf610bacea20842f60880d
Holger Döring <doering@uni-bremen.de>
2025-01-17 10:54:58 +0100

    Revise R packages install
    - add packages install script
      - use lock file or install recent versions
      - catch lock file install error
    - use packages install script in
      - Docker build
      - run all R script
    - update packages update script
      - remove 'pak' install (check)
      - change structure

--- utils/r-packages-update.R
+++ utils/r-packages-update.R
@@ -2,18 +2,10 @@
 
 library(pak)
 
-# install 'pak' to use modern package installer
-if (!"pak" %in% installed.packages()) {
-  install.packages("pak", repos = getOption("repos")[[1]])
-}
-
-# get packages used in project folder
-pkg_install("renv", ask = FALSE, upgrade = FALSE)
+# get and install packages used in project folder
 deps <- unique(renv::dependencies()[["Package"]])
-
-# install project packages
 pkg_install(deps, ask = FALSE, upgrade = TRUE)
-# pak::lockfile_install(update = FALSE)
 
 # create lock file of versions for all packages
 lockfile_create(deps)
+lockfile_install(update = TRUE)
